1eb98d54421237a77077af9c4e28666b5cc5dc4d
SamuelCK <51674985+SamuelCK@users.noreply.github.com>
2019-11-25 15:46:27 -0300

    Add files via upload

---
+++ TrabalhoFinal_Stonks/stonks.py
@@ -0,0 +1,9 @@
+def stonks(mediam, opens, closes, datas, num):
+    tupl = []
+    for i in range(len(closes)-6, len(closes)):
+        if mediam[num][i] <= opens[num][i] or mediam[num][i] >= closes[num][i]:
+            if mediam[num[i]] > opens[num][i+5]:
+                tupl.append(tuple ([datas[num][i], mediam[num][i], 1]))
+            else:
+                tupl.append(tuple ([datas[num][i], mediam[num][i], 0]))
+    return tupl
